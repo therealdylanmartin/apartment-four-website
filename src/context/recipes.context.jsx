54eb1494b953acb3914e5a29421734ee9042ba36
Dylan Martin <dylanmmartin84@gmail.com>
2023-01-18 18:27:24 -0600

    Sorting recipes by date last updated unless dates match, then sorting by recipe number

--- src/context/recipes.context.jsx
+++ src/context/recipes.context.jsx
@@ -6,19 +6,38 @@ export const RecipesContext = createContext({
 })
 
 export const RecipesProvider = ({ children }) => {
+  // Set empty array for recipes
   const [recipes, setRecipes] = useState([]);
+  // Point to recipes reference for Provider value
   const value = { recipes };
 
   useEffect(() => {
+    // Create function to fetch recipes from Firestore database
     const getRecipes = async () => {
+      // Initiate fetch
       const fetchedRecipes = await getRecipesAndDocuments();
-      const sortedRecipes = fetchedRecipes.sort((a, b) => a.id - b.id);
+      // Once fetched, use sort() function to order recipes
+      const sortedRecipes = fetchedRecipes.sort((recipe1, recipe2) => {
+        // Get date string from incoming recipe objects date last updated
+        const date1 = recipe1.lastUpdated.slice(0, 10).replace(/-/g, '');
+        const date2 = recipe2.lastUpdated.slice(0, 10).replace(/-/g, '');
+        // If dates don't match, sort by newest date
+        if (date2 - date1 !== 0) {
+          return date2 - date1;
+        } else {
+          // Else sort by descending recipe number
+          return recipe2.recipeNum - recipe1.recipeNum;
+        }
+      })
+      // Set the recipes state with the sorted recipes object
       setRecipes(sortedRecipes);
     }
 
+    // Trigger the function to initiate recipes fetch from Firestore database
     getRecipes();
   }, [])
 
+  // Use the state recipes object as value for the context provider
   return (
     <RecipesContext.Provider value={value}>
       {children}
